dc94bb66af57817ca209bc46185e58d5fe0904c0
Damien Engels <engelsdamien@gmail.com>
2015-11-02 23:03:45 +0100

    CHICHI giving up for tonight

--- src/classification/main.m
+++ src/classification/main.m
@@ -7,6 +7,23 @@ addpath('../methods');
 
 load('../../data/SaoPaulo_classification.mat')
 
-% predict = @(Xtr, Ytr, Xte) predictRegression(Xtr, Ytr, Xte, 10^5);
-% [ trRMSE, teRMSE ] = crossValidation(y_train, X_train, 3, predict);
+predictor = @(y, tX) penLogisticRegression(y, tX, 0.01);
+predict = @(Xtr, ytr, Xte) predictClassification(Xtr, ytr, Xte, predictor);
 
+[yTe, pTe] = predict(X_train, y_train, X_test);
+csvwrite('results/predictions_classification.csv', pTe);
+
+
+nbIteration = 3;
+
+RMSE = zeros(1, nbIteration);
+for i = 1:nbIteration
+    home;
+    fprintf('Iteration %d out of %d\n', i, nbIteration);
+    [~, teError] = crossValidation(X_train, y_train, 3, predict, false);
+    RMSE(i) = mean(teError.RMSE);
+end
+
+file = fopen('results/test_errors_regression.csv', 'w');
+fprintf(file, 'loss,%d', round(mean(RMSE)));
+fclose(file);
